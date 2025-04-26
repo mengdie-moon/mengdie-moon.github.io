e2c517ad048c1f0b4b97cd37f7e512ba461f1591
MengdieMoon <3257053519@qq.com>
2025-04-26 15:45:49 +0800

    Site updated: 2025-04-26 15:45:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/20/uniapp开发指南/","2025/04/26/搭建属于你的免费博客!/","2025/04/21/uniapp登录注册页面开发实战/"];function toRandomPost(){
+var posts=["2025/04/20/uniapp开发指南/","2025/04/21/uniapp登录注册页面开发实战/","2025/04/26/搭建属于你的免费博客!/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
